8dc4e15c5aa3d5f8fb32968367ad3892a20e90dc
Vladimir Saldivar <spartdark@gmail.com>
2017-04-11 12:50:13 -0500

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { HeaderComponent } from './components/header.component';
+import { BodyComponent } from './components/body.component';
+import { FooterComponent } from './components/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent,HeaderComponent ],
+  declarations: [ AppComponent,
+    HeaderComponent,
+    BodyComponent,
+  FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
