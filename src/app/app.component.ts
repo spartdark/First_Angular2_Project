711ec016c9305dd4f38cb9430db329f918ebd27a
Vladimir Saldivar <spartdark@gmail.com>
2017-04-11 12:02:58 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html'
+  //template: `
+//<app-header></app-header>
+  //`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+  name = 'Hola Mundo!!';
+
+}
