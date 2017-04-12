d8d0f92fb63d2b73374d6c1675488b471cafaa25
Vladimir Saldivar <spartdark@gmail.com>
2017-04-12 13:51:01 -0500

    structure example

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+  visible: boolean = true;
+  /*
+*ngIf="visible" cuando es falso destruye el objet por completo
+  */
+  texto: string ="Un gran poder requiere una gran responsabilidad.";
+  texto2: string = "Primer proyecto Angular Spartdark";
+
+  personajes: string[] = ["Spiderman","Venom","Doc. Octopus"]
+  //ngFor
 }
